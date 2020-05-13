1038c98cfc2e653ea19a50407e35a3049c9f35c9
JulianSaturno <65220886+JulianSaturno@users.noreply.github.com>
2020-05-12 23:20:42 -0700

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -1,9 +1,9 @@
 from PIL import Image
-Image01 = Image.open("/home/saturnojulia/Python/Photobooth_Photos/image0.jpg").resize((450,450))
-Image02 = Image.open("/home/saturnojulia/Python/Photobooth_Photos/image1.jpg").resize((450,450))
-Image03 = Image.open("/home/saturnojulia/Python/Photobooth_Photos/image2.jpg").resize((450,450))
-Image04 = Image.open("/home/saturnojulia/Python/Photobooth_Photos/image3.jpg").resize((450,450))
-Logo = Image.open("/home/saturnojulia/Desktop/SuLogo.png")
+Image01 = Image.open("/home/YourName/Python/Photobooth_Photos/image0.jpg").resize((450,450))
+Image02 = Image.open("/home/YourName/Python/Photobooth_Photos/image1.jpg").resize((450,450))
+Image03 = Image.open("/home/YourName/Python/Photobooth_Photos/image2.jpg").resize((450,450))
+Image04 = Image.open("/home/YourName/Python/Photobooth_Photos/image3.jpg").resize((450,450))
+Logo = Image.open("/home/YourName/Desktop/SuLogo.png")
 Logo = Logo.resize((200,200))
 
 newImage = Image.new("RGB", (2000, 500), (0, 0, 0) )
@@ -15,4 +15,4 @@ newImage.paste(Image04,(1375,25))
 
 newImage.paste(Logo,(1800,300))
 
-newImage.save("/home/saturnojulia/Python/Photobooth_Photos/Saved.jpg")
+newImage.save("/home/YourName/Python/Photobooth_Photos/Saved.jpg")
